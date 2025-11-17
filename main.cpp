9d60f9233bbb34f0183eb95e7b5f15a9a7d0f976
F2RUZ <gaffarovferuz4@gmail.com>
2025-11-17 18:12:41 +0500

    c++

---
+++ main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+using namespace std ;
+int main(){
+
+// for (int i = 0; i < 5; i++)
+// {
+//     for (int j = 0; j < 5; j++)
+//     {
+//       cout << " *";
+//     }
+
+//     cout << endl;
+    
+// }
+
+
+
+// for (int i = 0; i < 4; i++)
+// {
+//     for (int j = 0; j < 6; j++)
+//     {
+//         cout << " *";
+//     }
+
+//     cout << endl;
+    
+// }
+
+
+
+
+for (int i = 1; i <= 5; i--)
+{
+    for (int j = 1; j <= i; j++)
+    {
+        cout << " *";
+    }
+
+    cout << endl;
+    
+}
+
+
+
+
+
+
+    return 0;
+}
