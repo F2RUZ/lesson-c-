317ffaa62b8e3b451489fc15148e1aed0e23e459
F2RUZ <gaffarovferuz4@gmail.com>
2025-11-19 17:50:17 +0500

    uchburchak

--- main.cpp
+++ main.cpp
@@ -1,49 +1,21 @@
 #include <iostream>
+using namespace std;
 
-using namespace std ;
-int main(){
-
-// for (int i = 0; i < 5; i++)
-// {
-//     for (int j = 0; j < 5; j++)
-//     {
-//       cout << " *";
-//     }
-
-//     cout << endl;
-    
-// }
+int main() {
+   
 
 
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
 
-// for (int i = 0; i < 4; i++)
-// {
-//     for (int j = 0; j < 6; j++)
-//     {
-//         cout << " *";
-//     }
-
-//     cout << endl;
-    
-// }
-
-
-
-
-for (int i = 1; i <= 5; i--)
-{
-    for (int j = 1; j <= i; j++)
-    {
-        cout << " *";
+            if (i == 5 || j == 1 || j == i) {
+                cout << " *";
+            } else {
+                cout << "  ";
+            }
+        }
+        cout << endl;
     }
-
-    cout << endl;
-    
-}
-
-
-
-
 
 
     return 0;
